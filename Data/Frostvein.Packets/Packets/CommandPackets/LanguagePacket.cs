0443fa13cfaeec1da3639720bfd814cc9af2e28c
KILL009 <gmthemaster@gmail.com>
2026-07-19 14:01:51 -0400

    feat(localization): add player language command packet

---
+++ Data/Frostvein.Packets/Packets/CommandPackets/LanguagePacket.cs
@@ -0,0 +1,14 @@
+using Frostvein.Core;
+using Frostvein.Domain;
+
+namespace Frostvein.Packets.Packets.CommandPackets
+{
+    [PacketHeader("$Language", PassNonParseablePacket = true, Authority = AuthorityType.User)]
+    public class LanguagePacket : PacketDefinition
+    {
+        [PacketIndex(0)]
+        public string Culture { get; set; }
+
+        public static string ReturnHelp() => "$Language <en|fr>";
+    }
+}
