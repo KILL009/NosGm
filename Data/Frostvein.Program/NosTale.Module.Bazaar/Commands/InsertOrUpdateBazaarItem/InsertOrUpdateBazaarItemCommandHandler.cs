a24f164802d69e01ef3bab5b8870661af5201cf2
KILL009 <gmthemaster@gmail.com>
2026-07-20 16:20:21 -0400

    refactor: centralize bazaar cache refresh

--- Data/Frostvein.Program/NosTale.Module.Bazaar/Commands/InsertOrUpdateBazaarItem/InsertOrUpdateBazaarItemCommandHandler.cs
+++ Data/Frostvein.Program/NosTale.Module.Bazaar/Commands/InsertOrUpdateBazaarItem/InsertOrUpdateBazaarItemCommandHandler.cs
@@ -2,7 +2,6 @@ using FluentValidation;
 using MediatR;
 using Frostvein.DAL;
 using Frostvein.Data;
-using Frostvein.GameObject;
 using Frostvein.GameObject.Modules.Bazaar.Commands;
 using System;
 using System.Threading;
@@ -22,12 +21,7 @@ namespace NosTale.Module.Bazaar.Commands.InsertOrUpdateBazaarItem
 
         public async Task<long> Handle(InsertOrUpdateBazaarItemCommand command, CancellationToken cancellationToken)
         {
-            await _commandValidator.ValidateAndThrowAsync(command);
-
-            if (command.BazaarItem == null)
-            {
-                return -1;
-            }
+            await _commandValidator.ValidateAndThrowAsync(command, cancellationToken);
 
             BazaarItemDTO item = command.BazaarItem;
             if (!command.RefreshOnly)
@@ -36,44 +30,14 @@ namespace NosTale.Module.Bazaar.Commands.InsertOrUpdateBazaarItem
             }
 
             long bazaarItemId = item.BazaarItemId;
-            lock (_bazaarManager.GetItemLock(bazaarItemId))
+            if (!_bazaarManager.TryRefreshListing(bazaarItemId, out string failure))
             {
-                if (command.RefreshOnly)
-                {
-                    item = DAOFactory.BazaarItemDAO.LoadById(bazaarItemId);
-                    if (item == null)
-                    {
-                        _bazaarManager.BazaarItems.TryRemove(bazaarItemId, out _);
-                        _bazaarManager.BazaarItemLinks.TryRemove(bazaarItemId, out _);
-                        return -1;
-                    }
-                }
-
-                var itemDto = DAOFactory.ItemInstanceDAO.LoadById(item.ItemInstanceId);
-                if (itemDto == null)
-                {
-                    _bazaarManager.BazaarItems.TryRemove(bazaarItemId, out _);
-                    _bazaarManager.BazaarItemLinks.TryRemove(bazaarItemId, out _);
-                    return -1;
-                }
-
-                var link = new BazaarItemLink
-                {
-                    BazaarItem = item,
-                    Item = new ItemInstance(itemDto),
-                    Owner = DAOFactory.CharacterDAO.LoadById(item.SellerId)?.Name
-                };
-
-                bool exists = _bazaarManager.BazaarItems.ContainsKey(bazaarItemId);
-                _bazaarManager.BazaarItems[bazaarItemId] = item;
-                _bazaarManager.BazaarItemLinks[bazaarItemId] = link;
-
-                Console.WriteLine(exists
-                    ? $"Updating item: {bazaarItemId}. CharacterId: {item.SellerId}"
-                    : $"Inserting item: {bazaarItemId}. CharacterId: {item.SellerId}");
-
-                return bazaarItemId;
+                Console.WriteLine($"Unable to refresh bazaar listing {bazaarItemId}: {failure}");
+                return -1;
             }
+
+            Console.WriteLine($"Bazaar listing {bazaarItemId} refreshed successfully.");
+            return bazaarItemId;
         }
     }
 }
