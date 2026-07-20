5b6d65e83682b6d60140f5357fba10098c2b2cd9
KILL009 <gmthemaster@gmail.com>
2026-07-19 23:10:46 -0400

    feat(trade): expose atomic persistence service

---
+++ Data/Frostvein.DAL/Frostvein.DAL/TradeCommitService.cs
@@ -0,0 +1,23 @@
+using Frostvein.DAL.DAO;
+using Frostvein.Data;
+using System;
+
+namespace Frostvein.DAL
+{
+    public sealed class TradeCommitService
+    {
+        private static readonly Lazy<TradeCommitService> LazyInstance =
+            new Lazy<TradeCommitService>(() => new TradeCommitService(new TradeCommitDAO()));
+
+        private readonly TradeCommitDAO _dao;
+
+        internal TradeCommitService(TradeCommitDAO dao)
+        {
+            _dao = dao ?? throw new ArgumentNullException(nameof(dao));
+        }
+
+        public static TradeCommitService Instance => LazyInstance.Value;
+
+        public TradeCommitResult Commit(TradeCommitDTO request) => _dao.Commit(request);
+    }
+}
