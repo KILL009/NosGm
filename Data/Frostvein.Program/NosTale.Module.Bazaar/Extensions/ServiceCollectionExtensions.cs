5626e4eba18e00eee7ec68668c9dfd4de1b11eee
KILL009 <gmthemaster@gmail.com>
2026-07-20 18:30:07 -0400

    fix: keep a single MediatR handler for global bazaar searches

--- Data/Frostvein.Program/NosTale.Module.Bazaar/Extensions/ServiceCollectionExtensions.cs
+++ Data/Frostvein.Program/NosTale.Module.Bazaar/Extensions/ServiceCollectionExtensions.cs
@@ -1,8 +1,5 @@
 ﻿using MediatR;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Frostvein.GameObject.Modules.Bazaar.Queries;
-using NosTale.Module.Bazaar.Queries.GetRcbList;
 using System;
 using System.Reflection;
 
@@ -14,10 +11,6 @@ namespace NosTale.Module.Bazaar.Extensions
         {
             Console.WriteLine("Initializing bazaar module");
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
-
-            services.RemoveAll<IRequestHandler<GetRcbListQuery, string>>();
-            services.AddTransient<IRequestHandler<GetRcbListQuery, string>, GetRcbListAuthoritativeQueryHandler>();
-
             return services;
         }
     }
