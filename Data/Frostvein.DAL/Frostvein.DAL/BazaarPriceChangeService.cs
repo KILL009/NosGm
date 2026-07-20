45f4ba49998c9efcfbc5562c3451664cb11eea23
KILL009 <gmthemaster@gmail.com>
2026-07-20 10:46:34 -0400

    feat(bazaar): expose atomic price change service

--- Data/Frostvein.DAL/Frostvein.DAL/BazaarPriceChangeService.cs
+++ Data/Frostvein.DAL/Frostvein.DAL/BazaarPriceChangeService.cs
@@ -1,6 +1,23 @@
+using Frostvein.DAL.DAO;
+using Frostvein.Data;
+using System;
+
 namespace Frostvein.DAL
 {
     public sealed class BazaarPriceChangeService
     {
+        private static readonly Lazy<BazaarPriceChangeService> LazyInstance =
+            new Lazy<BazaarPriceChangeService>(() => new BazaarPriceChangeService(new BazaarPriceChangeDAO()));
+
+        private readonly BazaarPriceChangeDAO _dao;
+
+        internal BazaarPriceChangeService(BazaarPriceChangeDAO dao)
+        {
+            _dao = dao ?? throw new ArgumentNullException(nameof(dao));
+        }
+
+        public static BazaarPriceChangeService Instance => LazyInstance.Value;
+
+        public BazaarPriceChangeResult Commit(BazaarPriceChangeDTO request) => _dao.Commit(request);
     }
 }
