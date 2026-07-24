a952028443c25b963bb195cefe8ecab81ce85790
KILL009 <gmthemaster@gmail.com>
2026-07-24 16:54:34 -0400

    perf: use indexed existence query for daily rewards

--- Data/NosGm.GameObject/Extension/Reward/Reward.cs
+++ Data/NosGm.GameObject/Extension/Reward/Reward.cs
@@ -1,29 +1,27 @@
-﻿using NosGm.Core;
-using NosGm.DAL;
+﻿using NosGm.DAL;
 using NosGm.Data;
-using NosGm.Domain;
-using NosGm.GameObject.Helpers;
 using NosGm.GameObject.Networking;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using static System.Collections.Specialized.BitVector32;
 
 namespace NosGm.GameObject.Extension
 {
     public static class RewardExtension
     {
+        private const string DailyRewardKey = "DAILY_REWARD";
+
         public static void DailyReward(ClientSession session)
         {
-            var isMartial = session.Character.Class.Equals(ClassType.MartialArtist);
-            var count = DAOFactory.GeneralLogDAO.LoadByAccount(session.Account.AccountId)
-                .Count(s => s.LogData == (isMartial ? "DAILY_REWARD" : "DAILY_REWARD") && s.Timestamp.Day >= DateTime.Now.Day);
-            if (count != 0)
+            DateTime today = DateTime.Now.Date;
+            if (DAOFactory.GeneralLogDAO.ExistsForAccount(
+                    session.Account.AccountId,
+                    DailyRewardKey,
+                    today,
+                    today.AddDays(1)))
             {
                 return;
             }
-            session.Character.GiftAdd((short)(isMartial ? 11008 : 11008), (short)(isMartial ? 1 : 1));
+
+            session.Character.GiftAdd(11008, 1);
             session.SendPacket(session.Character.GenerateSay("You claimed your Daily Reward!", 12));
 
             DAOFactory.GeneralLogDAO.Insert(new GeneralLogDTO
@@ -32,22 +30,41 @@ namespace NosGm.GameObject.Extension
                 CharacterId = session.Character.CharacterId,
                 Timestamp = DateTime.Now,
                 IpAddress = session.IpAddress,
-                LogData = isMartial ? "DAILY_REWARD" : "DAILY_REWARD",
+                LogData = DailyRewardKey,
                 LogType = "World"
             });
         }
 
-        public static void HandleMultipleItemDrops(ClientSession Session, MapMonster monsterToAttack, long? owner, DropDTO drop)
+        public static void HandleMultipleItemDrops(ClientSession session, MapMonster monsterToAttack, long? owner,
+            DropDTO drop)
         {
-            Session.CurrentMapInstance.DropItemByMonster(owner, drop, (short)(monsterToAttack.MapX + ServerManager.RandomNumber(-10, 10)), (short)(monsterToAttack.MapY + ServerManager.RandomNumber(-10, 10)));
-            Session.CurrentMapInstance.DropItemByMonster(owner, drop, (short)(monsterToAttack.MapX + ServerManager.RandomNumber(-10, 10)), (short)(monsterToAttack.MapY + ServerManager.RandomNumber(-10, 10)));
-            Session.CurrentMapInstance.DropItemByMonster(owner, drop, (short)(monsterToAttack.MapX + ServerManager.RandomNumber(-10, 10)), (short)(monsterToAttack.MapY + ServerManager.RandomNumber(-10, 10)));
-            Session.CurrentMapInstance.DropItemByMonster(owner, drop, (short)(monsterToAttack.MapX + ServerManager.RandomNumber(-10, 10)), (short)(monsterToAttack.MapY + ServerManager.RandomNumber(-10, 10)));
-            Session.CurrentMapInstance.DropItemByMonster(owner, drop, (short)(monsterToAttack.MapX + ServerManager.RandomNumber(-10, 10)), (short)(monsterToAttack.MapY + ServerManager.RandomNumber(-10, 10)));
-            Session.CurrentMapInstance.DropItemByMonster(owner, drop, (short)(monsterToAttack.MapX + ServerManager.RandomNumber(-10, 10)), (short)(monsterToAttack.MapY + ServerManager.RandomNumber(-10, 10)));
-            Session.CurrentMapInstance.DropItemByMonster(owner, drop, (short)(monsterToAttack.MapX + ServerManager.RandomNumber(-10, 10)), (short)(monsterToAttack.MapY + ServerManager.RandomNumber(-10, 10)));
-            Session.CurrentMapInstance.DropItemByMonster(owner, drop, (short)(monsterToAttack.MapX + ServerManager.RandomNumber(-10, 10)), (short)(monsterToAttack.MapY + ServerManager.RandomNumber(-10, 10)));
-            Session.CurrentMapInstance.DropItemByMonster(owner, drop, (short)(monsterToAttack.MapX + ServerManager.RandomNumber(-10, 10)), (short)(monsterToAttack.MapY + ServerManager.RandomNumber(-10, 10)));
+            session.CurrentMapInstance.DropItemByMonster(owner, drop,
+                (short)(monsterToAttack.MapX + ServerManager.RandomNumber(-10, 10)),
+                (short)(monsterToAttack.MapY + ServerManager.RandomNumber(-10, 10)));
+            session.CurrentMapInstance.DropItemByMonster(owner, drop,
+                (short)(monsterToAttack.MapX + ServerManager.RandomNumber(-10, 10)),
+                (short)(monsterToAttack.MapY + ServerManager.RandomNumber(-10, 10)));
+            session.CurrentMapInstance.DropItemByMonster(owner, drop,
+                (short)(monsterToAttack.MapX + ServerManager.RandomNumber(-10, 10)),
+                (short)(monsterToAttack.MapY + ServerManager.RandomNumber(-10, 10)));
+            session.CurrentMapInstance.DropItemByMonster(owner, drop,
+                (short)(monsterToAttack.MapX + ServerManager.RandomNumber(-10, 10)),
+                (short)(monsterToAttack.MapY + ServerManager.RandomNumber(-10, 10)));
+            session.CurrentMapInstance.DropItemByMonster(owner, drop,
+                (short)(monsterToAttack.MapX + ServerManager.RandomNumber(-10, 10)),
+                (short)(monsterToAttack.MapY + ServerManager.RandomNumber(-10, 10)));
+            session.CurrentMapInstance.DropItemByMonster(owner, drop,
+                (short)(monsterToAttack.MapX + ServerManager.RandomNumber(-10, 10)),
+                (short)(monsterToAttack.MapY + ServerManager.RandomNumber(-10, 10)));
+            session.CurrentMapInstance.DropItemByMonster(owner, drop,
+                (short)(monsterToAttack.MapX + ServerManager.RandomNumber(-10, 10)),
+                (short)(monsterToAttack.MapY + ServerManager.RandomNumber(-10, 10)));
+            session.CurrentMapInstance.DropItemByMonster(owner, drop,
+                (short)(monsterToAttack.MapX + ServerManager.RandomNumber(-10, 10)),
+                (short)(monsterToAttack.MapY + ServerManager.RandomNumber(-10, 10)));
+            session.CurrentMapInstance.DropItemByMonster(owner, drop,
+                (short)(monsterToAttack.MapX + ServerManager.RandomNumber(-10, 10)),
+                (short)(monsterToAttack.MapY + ServerManager.RandomNumber(-10, 10)));
         }
     }
 }
