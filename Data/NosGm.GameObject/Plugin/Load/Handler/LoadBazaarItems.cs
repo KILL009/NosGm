e0b8f7a62fc36603560bf31fa932e8e7bef7bb46
KILL009 <gmthemaster@gmail.com>
2026-07-24 18:31:05 -0400

    perf: remove bazaar startup N+1 queries

--- Data/NosGm.GameObject/Plugin/Load/Handler/LoadBazaarItems.cs
+++ Data/NosGm.GameObject/Plugin/Load/Handler/LoadBazaarItems.cs
@@ -3,8 +3,6 @@ using NosGm.DAL;
 using NosGm.Data;
 using NosGm.GameObject.Networking;
 using System;
-using System.Collections.Concurrent;
-using System.Threading.Tasks;
 
 namespace NosGm.GameObject.Plugin.Load
 {
@@ -15,26 +13,24 @@ namespace NosGm.GameObject.Plugin.Load
             try
             {
                 ServerManager.Instance.BazaarList = new ThreadSafeGenericList<BazaarItemLink>();
-                OrderablePartitioner<BazaarItemDTO> bazaarPartitioner = Partitioner.Create(DAOFactory.BazaarItemDAO.LoadAll(), EnumerablePartitionerOptions.NoBuffering);
-                Parallel.ForEach(bazaarPartitioner, new ParallelOptions { MaxDegreeOfParallelism = 8 }, bazaarItem =>
+
+                foreach (BazaarItemLoadDTO row in DAOFactory.BazaarItemDAO.LoadAllHydrated())
                 {
-                    BazaarItemLink item = new BazaarItemLink
+                    ServerManager.Instance.BazaarList.Add(new BazaarItemLink
                     {
-                        BazaarItem = bazaarItem
-                    };
-                    CharacterDTO chara = DAOFactory.CharacterDAO.LoadById(bazaarItem.SellerId);
-                    if (chara != null)
-                    {
-                        item.Owner = chara.Name;
-                        item.Item = new ItemInstance(DAOFactory.ItemInstanceDAO.LoadById(bazaarItem.ItemInstanceId));
-                    }
-                    ServerManager.Instance.BazaarList.Add(item);
-                });
-                LoggerService.LogServer.Logger.UpdateLoadOutput($"{ServerManager.Instance.BazaarList.Count} Bazaar Items - Status: Successful", Domain.LogType.LOAD);
+                        BazaarItem = row.BazaarItem,
+                        Owner = row.OwnerName,
+                        Item = row.ItemInstance == null ? null : new ItemInstance(row.ItemInstance)
+                    });
+                }
+
+                LoggerService.LogServer.Logger.UpdateLoadOutput(
+                    $"{ServerManager.Instance.BazaarList.Count} Bazaar Items - Status: Successful",
+                    Domain.LogType.LOAD);
             }
             catch (Exception ex)
             {
-                LoggerService.LogServer.Logger.LogAsync($"{ex.ToString()}", Domain.LogType.ERROR);
+                LoggerService.LogServer.Logger.LogAsync(ex.ToString(), Domain.LogType.ERROR);
                 throw;
             }
         }
