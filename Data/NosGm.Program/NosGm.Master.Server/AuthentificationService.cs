f8fe4d958b0b0a13bce114f2fb323d8bc0f3dd40
KILL009 <gmthemaster@gmail.com>
2026-07-26 23:41:59 -0400

    feat: register and consume one-time Gameforge tickets

--- Data/NosGm.Program/NosGm.Master.Server/AuthentificationService.cs
+++ Data/NosGm.Program/NosGm.Master.Server/AuthentificationService.cs
@@ -1,19 +1,21 @@
-﻿using NosGm.Configuration;
+using NosGm.Configuration;
 using NosGm.DAL;
 using NosGm.Data;
 using NosGm.Master.Library.Interface;
 using NosGm.SCS.Communication.ScsServices.Service;
-using System.Configuration;
+using System;
+using System.Linq;
 
 namespace NosGm.Master.Server
 {
     internal class AuthentificationService : ScsService, IAuthentificationService
     {
-        #region Methods
-
         public bool Authenticate(string authKey)
         {
-            if (string.IsNullOrWhiteSpace(authKey)) return false;
+            if (string.IsNullOrWhiteSpace(authKey))
+            {
+                return false;
+            }
 
             if (authKey == ServerConfiguration.AuthServiceKey)
             {
@@ -26,33 +28,86 @@ namespace NosGm.Master.Server
 
         public AccountDTO ValidateAccount(string userName, string passHash)
         {
-            if ( /*!MSManager.Instance.AuthentificatedClients.Any(s => s.Equals(CurrentClient.ClientId)) || */
-                string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(passHash)) return null;
+            if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(passHash))
+            {
+                return null;
+            }
 
-            var account = DAOFactory.AccountDAO.LoadByName(userName);
-
-            if (account?.Password == passHash) return account;
-            return null;
+            AccountDTO account = DAOFactory.AccountDAO.LoadByName(userName);
+            return account?.Password == passHash ? account : null;
         }
 
         public CharacterDTO ValidateAccountAndCharacter(string userName, string characterName, string passHash)
         {
-            if (!MSManager.Instance.AuthentificatedClients.Any(s => s.Equals(CurrentClient.ClientId)) ||
-                string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(characterName) ||
-                string.IsNullOrEmpty(passHash)) return null;
-
-            var account = DAOFactory.AccountDAO.LoadByName(userName);
-
-            if (account?.Password == passHash)
+            if (!IsAuthenticatedClient() || string.IsNullOrEmpty(userName) ||
+                string.IsNullOrEmpty(characterName) || string.IsNullOrEmpty(passHash))
             {
-                var character = DAOFactory.CharacterDAO.LoadByName(characterName);
-                if (character?.AccountId == account.AccountId) return character;
                 return null;
             }
 
-            return null;
+            AccountDTO account = DAOFactory.AccountDAO.LoadByName(userName);
+            if (account?.Password != passHash)
+            {
+                return null;
+            }
+
+            CharacterDTO character = DAOFactory.CharacterDAO.LoadByName(characterName);
+            return character?.AccountId == account.AccountId ? character : null;
         }
 
-        #endregion
+        public bool RegisterGameforgeAuthTicket(
+            string accountName,
+            string authToken,
+            string installationId,
+            byte countryId)
+        {
+            if (!IsAuthenticatedClient() ||
+                !Guid.TryParse(installationId, out Guid parsedInstallationId) ||
+                !GameforgeLoginPacketParser.TryGetCulture(countryId, out _))
+            {
+                return false;
+            }
+
+            AccountDTO account = DAOFactory.AccountDAO.LoadByName(accountName);
+            if (account == null || !string.Equals(account.Name, accountName, StringComparison.Ordinal))
+            {
+                return false;
+            }
+
+            int configuredTtl = ServerConfiguration.GameforgeAuthTicketTtlSeconds;
+            int ttlSeconds = Math.Max(15, Math.Min(600, configuredTtl));
+            return GameforgeAuthTicketStore.Instance.TryIssue(
+                account.Name,
+                authToken,
+                parsedInstallationId,
+                countryId,
+                TimeSpan.FromSeconds(ttlSeconds));
+        }
+
+        public string ConsumeGameforgeAuthTicket(
+            string authToken,
+            string installationId,
+            byte countryId)
+        {
+            if (!IsAuthenticatedClient() ||
+                !Guid.TryParse(installationId, out Guid parsedInstallationId))
+            {
+                return null;
+            }
+
+            return GameforgeAuthTicketStore.Instance.TryConsume(
+                authToken,
+                parsedInstallationId,
+                countryId,
+                out string accountName)
+                ? accountName
+                : null;
+        }
+
+        private bool IsAuthenticatedClient()
+        {
+            return MSManager.Instance.AuthentificatedClients.Any(
+                clientId => clientId.Equals(CurrentClient.ClientId));
+        }
     }
 }
