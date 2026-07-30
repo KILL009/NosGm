6ba3065114b73a1cce67373da3b26953573ea29d
KILL009 <gmthemaster@gmail.com>
2026-07-30 14:09:19 -0400

    Keep callback mirror failures isolated from SCS

--- Data/NosGm.Program/NosGm.Master.Server/MirroredCommunicationService.cs
+++ Data/NosGm.Program/NosGm.Master.Server/MirroredCommunicationService.cs
@@ -1,6 +1,8 @@
+using NosGm.Core;
 using NosGm.Domain;
 using NosGm.Master.Library.Interface;
 using System;
+using System.Linq;
 
 namespace NosGm.Master.Server
 {
@@ -16,68 +18,91 @@ namespace NosGm.Master.Server
                 string worldGroup = MSManager.Instance.WorldServers
                     .Find(world => world.Id == worldId)
                     ?.WorldGroup;
-                MasterCommunicationCallbackMirror.Instance
-                    .TryCharacterPresence(
-                        worldGroup,
-                        characterId,
-                        true);
+                Mirror(
+                    "CharacterConnected",
+                    () => MasterCommunicationCallbackMirror.Instance
+                        .TryCharacterPresence(
+                            worldGroup,
+                            characterId,
+                            true));
             }
             return connected;
         }
 
         public new void DisconnectCharacter(Guid worldId, long characterId)
         {
+            bool wasConnected = MSManager.Instance.ConnectedAccounts.Any(
+                account =>
+                    account.CharacterId == characterId &&
+                    account.ConnectedWorld?.Id == worldId);
             string worldGroup = MSManager.Instance.WorldServers
                 .Find(world => world.Id == worldId)
                 ?.WorldGroup;
             base.DisconnectCharacter(worldId, characterId);
-            MasterCommunicationCallbackMirror.Instance
-                .TryCharacterPresence(
-                    worldGroup,
-                    characterId,
-                    false);
+            if (wasConnected)
+            {
+                Mirror(
+                    "CharacterDisconnected",
+                    () => MasterCommunicationCallbackMirror.Instance
+                        .TryCharacterPresence(
+                            worldGroup,
+                            characterId,
+                            false));
+            }
         }
 
         public new void KickSession(long? accountId, int? sessionId)
         {
             base.KickSession(accountId, sessionId);
-            MasterCommunicationCallbackMirror.Instance
-                .TryKickSession(accountId, sessionId);
+            Mirror(
+                "KickSession",
+                () => MasterCommunicationCallbackMirror.Instance
+                    .TryKickSession(accountId, sessionId));
         }
 
         public new void RefreshPenalty(int penaltyId)
         {
             base.RefreshPenalty(penaltyId);
-            MasterCommunicationCallbackMirror.Instance
-                .TryPenaltyRefresh(penaltyId);
+            Mirror(
+                "UpdatePenaltyLog",
+                () => MasterCommunicationCallbackMirror.Instance
+                    .TryPenaltyRefresh(penaltyId));
         }
 
         public new void Restart(string worldGroup, int time = 5)
         {
             base.Restart(worldGroup, time);
-            MasterCommunicationCallbackMirror.Instance
-                .TryRestart(worldGroup, time);
+            Mirror(
+                "Restart",
+                () => MasterCommunicationCallbackMirror.Instance
+                    .TryRestart(worldGroup, time));
         }
 
         public new void RunGlobalEvent(EventType eventType, byte value)
         {
             base.RunGlobalEvent(eventType, value);
-            MasterCommunicationCallbackMirror.Instance
-                .TryGlobalEvent(eventType, value);
+            Mirror(
+                "RunGlobalEvent",
+                () => MasterCommunicationCallbackMirror.Instance
+                    .TryGlobalEvent(eventType, value));
         }
 
         public new void Shutdown(string worldGroup)
         {
             base.Shutdown(worldGroup);
-            MasterCommunicationCallbackMirror.Instance
-                .TryShutdown(worldGroup);
+            Mirror(
+                "Shutdown",
+                () => MasterCommunicationCallbackMirror.Instance
+                    .TryShutdown(worldGroup));
         }
 
         public new void UpdateBazaar(string worldGroup, long bazaarItemId)
         {
             base.UpdateBazaar(worldGroup, bazaarItemId);
-            MasterCommunicationCallbackMirror.Instance
-                .TryBazaarRefresh(worldGroup, bazaarItemId);
+            Mirror(
+                "UpdateBazaar",
+                () => MasterCommunicationCallbackMirror.Instance
+                    .TryBazaarRefresh(worldGroup, bazaarItemId));
         }
 
         public new void UpdateFamily(
@@ -86,18 +111,37 @@ namespace NosGm.Master.Server
             bool changeFaction)
         {
             base.UpdateFamily(worldGroup, familyId, changeFaction);
-            MasterCommunicationCallbackMirror.Instance
-                .TryFamilyRefresh(
-                    worldGroup,
-                    familyId,
-                    changeFaction);
+            Mirror(
+                "UpdateFamily",
+                () => MasterCommunicationCallbackMirror.Instance
+                    .TryFamilyRefresh(
+                        worldGroup,
+                        familyId,
+                        changeFaction));
         }
 
         public new void UpdateRelation(string worldGroup, long relationId)
         {
             base.UpdateRelation(worldGroup, relationId);
-            MasterCommunicationCallbackMirror.Instance
-                .TryRelationRefresh(worldGroup, relationId);
+            Mirror(
+                "UpdateRelation",
+                () => MasterCommunicationCallbackMirror.Instance
+                    .TryRelationRefresh(worldGroup, relationId));
+        }
+
+        private static void Mirror(string operation, Func<bool> enqueue)
+        {
+            try
+            {
+                enqueue();
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(
+                    "[CALLBACK_MIRROR_ISOLATED_FAILURE] Operation=" + operation +
+                    " SCS remains authoritative.",
+                    ex);
+            }
         }
     }
 }
