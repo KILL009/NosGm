a878a4ff64d361221af075a0c23ce3222e3a936e
KILL009 <gmthemaster@gmail.com>
2026-07-30 15:28:28 -0400

    Mirror character presence to exact peer Worlds

--- Data/NosGm.Program/NosGm.Master.Server/MirroredCommunicationService.cs
+++ Data/NosGm.Program/NosGm.Master.Server/MirroredCommunicationService.cs
@@ -1,7 +1,9 @@
+using NosGm.Communication.Client;
 using NosGm.Core;
 using NosGm.Domain;
 using NosGm.Master.Library.Interface;
 using System;
+using System.Linq;
 
 namespace NosGm.Master.Server
 {
@@ -9,6 +11,34 @@ namespace NosGm.Master.Server
         : CommunicationService,
           ICommunicationService
     {
+        public new bool ConnectCharacter(Guid worldId, long characterId)
+        {
+            Guid[] peerWorldIds = ResolvePeerWorldIds(worldId);
+            bool connected = base.ConnectCharacter(worldId, characterId);
+            if (connected)
+            {
+                MirrorPresence(peerWorldIds, characterId, true);
+            }
+            return connected;
+        }
+
+        public new void DisconnectCharacter(Guid worldId, long characterId)
+        {
+            bool wasConnected = MSManager.Instance.ConnectedAccounts.Any(
+                account =>
+                    account.CharacterId == characterId &&
+                    account.ConnectedWorld?.Id == worldId);
+            Guid[] peerWorldIds = wasConnected
+                ? ResolvePeerWorldIds(worldId)
+                : Array.Empty<Guid>();
+
+            base.DisconnectCharacter(worldId, characterId);
+            if (wasConnected)
+            {
+                MirrorPresence(peerWorldIds, characterId, false);
+            }
+        }
+
         public new void KickSession(long? accountId, int? sessionId)
         {
             base.KickSession(accountId, sessionId);
@@ -85,6 +115,38 @@ namespace NosGm.Master.Server
                 "UpdateRelation",
                 () => MasterCommunicationCallbackMirror.Instance
                     .TryRelationRefresh(worldGroup, relationId));
+        }
+
+        private static Guid[] ResolvePeerWorldIds(Guid sourceWorldId)
+        {
+            return CharacterPresenceMirrorRoutePlanner.ResolvePeerWorldIds(
+                    MSManager.Instance.WorldServers.Select(
+                        world => new CommunicationCallbackWorldRoute
+                        {
+                            WorldId = world.Id,
+                            WorldGroup = world.WorldGroup
+                        }),
+                    sourceWorldId)
+                .ToArray();
+        }
+
+        private static void MirrorPresence(
+            Guid[] peerWorldIds,
+            long characterId,
+            bool connected)
+        {
+            foreach (Guid targetWorldId in peerWorldIds)
+            {
+                Mirror(
+                    connected
+                        ? "CharacterConnected"
+                        : "CharacterDisconnected",
+                    () => MasterCommunicationCallbackMirror.Instance
+                        .TryCharacterPresence(
+                            targetWorldId,
+                            characterId,
+                            connected));
+            }
         }
 
         private static void Mirror(string operation, Func<bool> enqueue)
