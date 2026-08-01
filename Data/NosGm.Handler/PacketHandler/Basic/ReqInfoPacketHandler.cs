450ffeca8edac2c279d08576400aaf98b3d36f76
KILL009 <gmthemaster@gmail.com>
2026-08-01 12:40:07 -0400

    Handle modern req_info entity identifiers

--- Data/NosGm.Handler/PacketHandler/Basic/ReqInfoPacketHandler.cs
+++ Data/NosGm.Handler/PacketHandler/Basic/ReqInfoPacketHandler.cs
@@ -1,10 +1,9 @@
-﻿using NosGm.Packets.Packets.ClientPackets;
-using NosGm.Core;
+﻿using NosGm.Core;
 using NosGm.Domain;
 using NosGm.GameObject;
 using NosGm.GameObject.Networking;
+using NosGm.Packets.Packets.ClientPackets;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace NosGm.Handler.PacketHandler.Basic
 {
@@ -29,51 +28,113 @@ namespace NosGm.Handler.PacketHandler.Basic
 
         public void ReqInfo(ReqInfoPacket reqInfoPacket)
         {
-            if (Session.Character != null)
+            if (Session.Character == null)
             {
-                if (reqInfoPacket.Type == 6)
-                {
-                    if (reqInfoPacket.MateVNum.HasValue)
-                    {
-                        var mate = Session.CurrentMapInstance?.Sessions?.FirstOrDefault(s =>
-                                s?.Character?.Mates != null && s.Character.Mates.Any(o =>
-                                    o.MateTransportId == reqInfoPacket.MateVNum.Value))?
-                            .Character.Mates.FirstOrDefault(m => m.MateTransportId == reqInfoPacket.MateVNum.Value);
+                return;
+            }
 
-                        if (mate != null)
-                        {
-                            Session.SendPacket(mate.GenerateEInfo());
-                        }
-                    }
-                }
-                else if (reqInfoPacket.Type == 5)
-                {
-                    var npc = ServerManager.GetNpcMonster((short)reqInfoPacket.TargetVNum);
+            if (reqInfoPacket.Type == 6)
+            {
+                HandleModernEntityInfo(reqInfoPacket);
+                return;
+            }
 
-                    if (Session.CurrentMapInstance?.GetMonsterById(Session.Character.LastNpcMonsterId) is MapMonster monster && monster.Monster?.OriginalNpcMonsterVNum == reqInfoPacket.TargetVNum)
-                    {
-                        npc = ServerManager.GetNpcMonster(monster.Monster.NpcMonsterVNum);
-                    }    
+            if (reqInfoPacket.Type == 5)
+            {
+                var npc = ServerManager.GetNpcMonster((short)reqInfoPacket.TargetTypeOrVNum);
 
-                    if (npc != null)
-                    {
-                        Session.SendPacket(npc.GenerateEInfo());
-                    }
-                }
-                else if (reqInfoPacket.Type == 12)
+                if (Session.CurrentMapInstance?.GetMonsterById(Session.Character.LastNpcMonsterId) is MapMonster monster &&
+                    monster.Monster?.OriginalNpcMonsterVNum == reqInfoPacket.TargetTypeOrVNum)
                 {
-                    if (Session.Character.Inventory != null)
-                    {
-                        Session.SendPacket(Session.Character.Inventory.LoadBySlotAndType((short)reqInfoPacket.TargetVNum, InventoryType.Equipment)?.GenerateReqInfo());
-                    }
+                    npc = ServerManager.GetNpcMonster(monster.Monster.NpcMonsterVNum);
                 }
-                else
+
+                if (npc != null)
                 {
-                    if (ServerManager.Instance.GetSessionByCharacterId(reqInfoPacket.TargetVNum)?.Character is Character character)
+                    Session.SendPacket(npc.GenerateEInfo());
+                }
+
+                return;
+            }
+
+            if (reqInfoPacket.Type == 12)
+            {
+                if (Session.Character.Inventory != null)
+                {
+                    Session.SendPacket(Session.Character.Inventory
+                        .LoadBySlotAndType((short)reqInfoPacket.TargetTypeOrVNum, InventoryType.Equipment)
+                        ?.GenerateReqInfo());
+                }
+
+                return;
+            }
+
+            if (ServerManager.Instance.GetSessionByCharacterId(reqInfoPacket.TargetTypeOrVNum)?.Character is Character character)
+            {
+                Session.SendPacket(character.GenerateReqInfo());
+            }
+        }
+
+        private void HandleModernEntityInfo(ReqInfoPacket reqInfoPacket)
+        {
+            if (!reqInfoPacket.TargetId.HasValue || Session.CurrentMapInstance == null)
+            {
+                return;
+            }
+
+            var targetId = reqInfoPacket.TargetId.Value;
+
+            switch (reqInfoPacket.TargetTypeOrVNum)
+            {
+                case 1:
+                    if (ServerManager.Instance.GetSessionByCharacterId(targetId)?.Character is Character character)
                     {
                         Session.SendPacket(character.GenerateReqInfo());
                     }
-                }
+
+                    return;
+
+                case 2:
+                    var mapNpc = Session.CurrentMapInstance.Npcs.FirstOrDefault(npc => npc.MapNpcId == targetId);
+                    if (mapNpc != null)
+                    {
+                        var npcInfo = ServerManager.GetNpcMonster(mapNpc.NpcVNum);
+                        if (npcInfo != null)
+                        {
+                            Session.Character.LastNpcMonsterId = mapNpc.MapNpcId;
+                            Session.SendPacket(npcInfo.GenerateEInfo(useClientLocalizedName: true));
+                            return;
+                        }
+                    }
+
+                    var mate = Session.CurrentMapInstance.Sessions.FirstOrDefault(session =>
+                            session?.Character?.Mates != null && session.Character.Mates.Any(candidate =>
+                                candidate.MateTransportId == targetId))?
+                        .Character.Mates.FirstOrDefault(candidate => candidate.MateTransportId == targetId);
+
+                    if (mate != null)
+                    {
+                        Session.SendPacket(mate.GenerateEInfo());
+                    }
+
+                    return;
+
+                case 3:
+                    var monster = Session.CurrentMapInstance.GetMonsterById(targetId);
+                    if (monster == null)
+                    {
+                        return;
+                    }
+
+                    var monsterInfo = monster.Monster ?? ServerManager.GetNpcMonster(monster.MonsterVNum);
+                    if (monsterInfo == null)
+                    {
+                        return;
+                    }
+
+                    Session.Character.LastNpcMonsterId = monster.MapMonsterId;
+                    Session.SendPacket(monsterInfo.GenerateEInfo(useClientLocalizedName: true));
+                    return;
             }
         }
 
