09301493fcc2d5b09c58bc3f32fc05a8aeb83ebf
KILL009 <gmthemaster@gmail.com>
2026-08-03 19:35:07 -0400

    Make Glacernon ship cycles asynchronous and resilient

--- Data/NosGm.GameObject/Plugin/Event/Handler/GameEvent/Glacernon/GlacernonShip.cs
+++ Data/NosGm.GameObject/Plugin/Event/Handler/GameEvent/Glacernon/GlacernonShip.cs
@@ -1,50 +1,129 @@
-﻿using NosGm.Configuration;
+using NosGm.Configuration;
 using NosGm.Core;
 using NosGm.Core.Extensions;
 using NosGm.Domain;
 using NosGm.GameObject.Characters.Events;
 using NosGm.GameObject.Helpers;
 using NosGm.GameObject.Networking;
-using NosGm.GameObject.Plugin.Event.Handler;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reactive.Disposables;
-using System.Reactive.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace NosGm.GameObject.Plugin.Event
 {
-    public class GlacernonShip
+    public static class GlacernonShip
     {
-        #region Methods
-
         public static EventType EventType => EventType.GLACERNONSHIP;
 
         public static void GenerateGlacernonShip(byte faction)
         {
-            EventHelper.Instance.RunEvent(new EventContainer(ServerManager.GetMapInstance(ServerManager.GetBaseMapInstanceIdByMapId(145)), EventActionType.NPCSEFFECTCHANGESTATE, true));
-            var result = TimeExtensions.RoundUp(DateTime.Now, TimeSpan.FromMinutes(1));
-            Observable.Timer(result - DateTime.Now).Subscribe(X => GlacernonShipThread.Run(faction));
-        }
-
-        #endregion
-    }
-
-    public static class GlacernonShipThread
-    {
-        #region Methods
-
-        public static void Run(byte faction)
-        {
-            var map = ServerManager.GenerateMapInstance(149, faction == 1 ? MapInstanceType.Act4ShipAngel : MapInstanceType.Act4ShipDemon, new InstanceBag());
-            if (map == null)
+            if (faction != 1 && faction != 2)
             {
+                Logger.Warn($"[GLACERNON_SHIP] Result=Rejected Faction={faction}");
                 return;
             }
 
-            var mapNpc1 = new MapNpc
+            EventHelper.Instance.RunEvent(
+                new EventContainer(
+                    ServerManager.GetMapInstance(
+                        ServerManager.GetBaseMapInstanceIdByMapId(145)),
+                    EventActionType.NPCSEFFECTCHANGESTATE,
+                    true));
+
+            DateTime nextMinute = TimeExtensions.RoundUp(
+                DateTime.Now,
+                TimeSpan.FromMinutes(1));
+            TimeSpan delay = nextMinute - DateTime.Now;
+            Task.Run(async () =>
+            {
+                try
+                {
+                    await Task.Delay(delay).ConfigureAwait(false);
+                    await GlacernonShipRuntime.RunAsync(faction).ConfigureAwait(false);
+                }
+                catch (Exception exception)
+                {
+                    Logger.Error(
+                        $"[GLACERNON_SHIP] Result=Failed Faction={faction}",
+                        exception);
+                }
+            });
+        }
+    }
+
+    internal static class GlacernonShipRuntime
+    {
+        public static async Task RunAsync(byte faction)
+        {
+            MapInstance map = ServerManager.GenerateMapInstance(
+                149,
+                faction == 1
+                    ? MapInstanceType.Act4ShipAngel
+                    : MapInstanceType.Act4ShipDemon,
+                new InstanceBag());
+            if (map == null)
+            {
+                throw new InvalidOperationException(
+                    $"Glacernon ship map could not be created for faction {faction}.");
+            }
+
+            AddShipNpcs(map);
+            Logger.Info(
+                $"[GLACERNON_SHIP] Result=Started Faction={faction} " +
+                $"MapInstance={map.MapInstanceId}");
+
+            while (true)
+            {
+                try
+                {
+                    OpenShip();
+                    await Task.Delay(TimeSpan.FromMinutes(1)).ConfigureAwait(false);
+                    map.Broadcast(
+                        UserInterfaceHelper.GenerateMsg(
+                            Language.Instance.GetMessageFromKey("SHIP_MINUTE"),
+                            0));
+                    await Task.Delay(TimeSpan.FromSeconds(30)).ConfigureAwait(false);
+                    map.Broadcast(
+                        UserInterfaceHelper.GenerateMsg(
+                            string.Format(
+                                Language.Instance.GetMessageFromKey("SHIP_SECONDS"),
+                                30),
+                            0));
+                    await Task.Delay(TimeSpan.FromSeconds(20)).ConfigureAwait(false);
+                    map.Broadcast(
+                        UserInterfaceHelper.GenerateMsg(
+                            string.Format(
+                                Language.Instance.GetMessageFromKey("SHIP_SECONDS"),
+                                10),
+                            0));
+                    await Task.Delay(TimeSpan.FromSeconds(10)).ConfigureAwait(false);
+                    map.Broadcast(
+                        UserInterfaceHelper.GenerateMsg(
+                            Language.Instance.GetMessageFromKey("SHIP_SETOFF"),
+                            0));
+
+                    List<ClientSession> sessions = map.Sessions
+                        .Where(session => session?.Character != null)
+                        .ToList();
+                    TeleportPlayers(sessions);
+                    Logger.Info(
+                        $"[GLACERNON_SHIP] Result=Departed Faction={faction} " +
+                        $"Passengers={sessions.Count}");
+                }
+                catch (Exception exception)
+                {
+                    Logger.Error(
+                        $"[GLACERNON_SHIP] Result=CycleFailed Faction={faction}",
+                        exception);
+                    await Task.Delay(TimeSpan.FromSeconds(5)).ConfigureAwait(false);
+                }
+            }
+        }
+
+        private static void AddShipNpcs(MapInstance map)
+        {
+            var captain = new MapNpc
             {
                 NpcVNum = 613,
                 MapNpcId = map.GetNextNpcId(),
@@ -56,9 +135,10 @@ namespace NosGm.GameObject.Plugin.Event
                 Position = 1,
                 IsSitting = false
             };
-            mapNpc1.Initialize(map);
-            map.AddNPC(mapNpc1);
-            var mapNpc2 = new MapNpc
+            captain.Initialize(map);
+            map.AddNPC(captain);
+
+            var crew = new MapNpc
             {
                 NpcVNum = 540,
                 MapNpcId = map.GetNextNpcId(),
@@ -70,65 +150,74 @@ namespace NosGm.GameObject.Plugin.Event
                 Position = 3,
                 IsSitting = false
             };
-            mapNpc2.Initialize(map);
-            map.AddNPC(mapNpc2);
-            while (true)
+            crew.Initialize(map);
+            map.AddNPC(crew);
+        }
+
+        private static void OpenShip()
+        {
+            EventHelper.Instance.RunEvent(
+                new EventContainer(
+                    ServerManager.GetMapInstance(
+                        ServerManager.GetBaseMapInstanceIdByMapId(145)),
+                    EventActionType.NPCSEFFECTCHANGESTATE,
+                    false));
+        }
+
+        private static void TeleportPlayers(IEnumerable<ClientSession> sessions)
+        {
+            foreach (ClientSession session in sessions)
             {
-                openShip();
-                Thread.Sleep(60 * 1000);
-                map.Broadcast(UserInterfaceHelper.GenerateMsg(Language.Instance.GetMessageFromKey("SHIP_MINUTE"), 0));
-                // lockShip();
-                Thread.Sleep(30 * 1000);
-                map.Broadcast(UserInterfaceHelper.GenerateMsg(string.Format(Language.Instance.GetMessageFromKey("SHIP_SECONDS"), 30), 0));
-                Thread.Sleep(20 * 1000);
-                map.Broadcast(UserInterfaceHelper.GenerateMsg(string.Format(Language.Instance.GetMessageFromKey("SHIP_SECONDS"), 10), 0));
-                Thread.Sleep(10 * 1000);
-                map.Broadcast(UserInterfaceHelper.GenerateMsg(Language.Instance.GetMessageFromKey("SHIP_SETOFF"), 0));
-                var sessions = map.Sessions.Where(s => s?.Character != null).ToList();
-                Observable.Timer(TimeSpan.FromSeconds(0)).Subscribe(X => teleportPlayers(sessions));
+                if (session?.Character == null)
+                {
+                    continue;
+                }
+
+                if (!ServerManager.Instance.IsAct4Online())
+                {
+                    ServerManager.Instance.ChangeMap(
+                        session.Character.CharacterId,
+                        145,
+                        51,
+                        41);
+                    session.SendPacket(
+                        UserInterfaceHelper.GenerateInfo(
+                            Language.Instance.GetMessageFromKey("ACT4_OFFLINE")));
+                    continue;
+                }
+
+                switch (session.Character.Faction)
+                {
+                    case FactionType.None:
+                        ServerManager.Instance.ChangeMap(
+                            session.Character.CharacterId,
+                            145,
+                            51,
+                            41);
+                        session.SendPacket(
+                            UserInterfaceHelper.GenerateInfo(
+                                "You need to be part of a faction to join Act 4"));
+                        continue;
+                    case FactionType.Angel:
+                        session.Character.MapId = 130;
+                        session.Character.MapX = 12;
+                        session.Character.MapY = 40;
+                        break;
+                    case FactionType.Demon:
+                        session.Character.MapId = 131;
+                        session.Character.MapX = 12;
+                        session.Character.MapY = 40;
+                        break;
+                    default:
+                        continue;
+                }
+
+                session.Character.Event.EmitEvent(
+                    new PlayerChangeChannelEvent(
+                        ServerConfiguration.IPAddress,
+                        Convert.ToInt32(ServerConfiguration.GlacernonServerPort),
+                        3));
             }
         }
-        private static void openShip()
-        {
-            EventHelper.Instance.RunEvent(new EventContainer(ServerManager.GetMapInstance(ServerManager.GetBaseMapInstanceIdByMapId(145)), EventActionType.NPCSEFFECTCHANGESTATE, false));
-        }
-
-        private static void teleportPlayers(List<ClientSession> sessions)
-        {
-            foreach (var session in sessions)
-            {
-                if (ServerManager.Instance.IsAct4Online())
-                {
-                    switch (session.Character.Faction)
-                    {
-                        case FactionType.None:
-                            ServerManager.Instance.ChangeMap(session.Character.CharacterId, 145, 51, 41);
-                            session.SendPacket(UserInterfaceHelper.GenerateInfo("You need to be part of a faction to join Act 4"));
-                            return;
-
-                        case FactionType.Angel:
-                            session.Character.MapId = 130;
-                            session.Character.MapX = 12;
-                            session.Character.MapY = 40;
-                            break;
-
-                        case FactionType.Demon:
-                            session.Character.MapId = 131;
-                            session.Character.MapX = 12;
-                            session.Character.MapY = 40;
-                            break;
-                    }
-
-                    session.Character.Event.EmitEvent(new PlayerChangeChannelEvent(ServerConfiguration.IPAddress, Convert.ToInt32(ServerConfiguration.GlacernonServerPort), 3));
-                }
-                else
-                {
-                    ServerManager.Instance.ChangeMap(session.Character.CharacterId, 145, 51, 41); //145 51 41
-                    session.SendPacket(UserInterfaceHelper.GenerateInfo(Language.Instance.GetMessageFromKey("ACT4_OFFLINE")));
-                }
-            }
-        }
-
-        #endregion
     }
 }
