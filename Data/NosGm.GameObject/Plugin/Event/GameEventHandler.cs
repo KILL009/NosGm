480945242848219b47ab5b83bff0ea8c6488b458
KILL009 <gmthemaster@gmail.com>
2026-08-03 19:27:31 -0400

    Fix event runtime logger call

--- Data/NosGm.GameObject/Plugin/Event/GameEventHandler.cs
+++ Data/NosGm.GameObject/Plugin/Event/GameEventHandler.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Threading.Tasks;
 using Game.Configuration;
 using NosGm.Core;
@@ -47,9 +47,9 @@ namespace NosGm.GameObject.Plugin.Event
                 {
                     CompleteEvent(type);
                     Logger.Error(
-                        exception,
                         $"[EVENT_RUNTIME] Event={type} Result=Failed " +
-                        $"Channel={ServerManager.Instance.ChannelId} LevelBracket={LvlBracket} Value={value}");
+                        $"Channel={ServerManager.Instance.ChannelId} LevelBracket={LvlBracket} Value={value}",
+                        exception);
                 }
             });
         }
@@ -70,119 +70,54 @@ namespace NosGm.GameObject.Plugin.Event
                     ServerManager.Instance.RefreshRanking();
                     CompleteEvent(type);
                     break;
-
                 case EventType.LOD:
-                    if (ServerManager.Instance.ChannelId != 51)
-                    {
-                        LandOfDeath.GenerateLandOfDeath();
-                    }
-                    else
-                    {
-                        CompleteEvent(type);
-                    }
+                    if (ServerManager.Instance.ChannelId != 51) LandOfDeath.GenerateLandOfDeath();
+                    else CompleteEvent(type);
                     break;
-
                 case EventType.MINILANDREFRESHEVENT:
                     MinilandRefresh.GenerateMinilandEvent();
                     break;
-
                 case EventType.DAILYMISSIONEXTENSIONREFRESH:
                     ServerManager.Instance.RefreshDailyMissions();
                     CompleteEvent(type);
                     break;
-
                 case EventType.INSTANTBATTLE:
-                    if (ServerManager.Instance.ChannelId != 51)
-                    {
-                        InstantBattleRuntime.GenerateInstantBattle();
-                    }
-                    else
-                    {
-                        CompleteEvent(type);
-                    }
+                    if (ServerManager.Instance.ChannelId != 51) InstantBattleRuntime.GenerateInstantBattle();
+                    else CompleteEvent(type);
                     break;
-
                 case EventType.RAINBOWBATTLE:
-                    if (ServerManager.Instance.ChannelId != 51)
-                    {
-                        RainbowBattle.GenerateEvent();
-                    }
-                    else
-                    {
-                        CompleteEvent(type);
-                    }
+                    if (ServerManager.Instance.ChannelId != 51) RainbowBattle.GenerateEvent();
+                    else CompleteEvent(type);
                     break;
-
                 case EventType.GLACERNONSHIP:
                     GlacernonShip.GenerateGlacernonShip(1);
                     GlacernonShip.GenerateGlacernonShip(2);
                     break;
-
                 case EventType.TALENTARENA:
-                    if (ServerManager.Instance.ChannelId != 51)
-                    {
-                        ArenaEvent.GenerateTalentArena();
-                    }
-                    else
-                    {
-                        CompleteEvent(type);
-                    }
+                    if (ServerManager.Instance.ChannelId != 51) ArenaEvent.GenerateTalentArena();
+                    else CompleteEvent(type);
                     break;
-
                 case EventType.CALIGOR:
-                    if (ServerManager.Instance.ChannelId == 51)
-                    {
-                        CaligorRaid.Run();
-                    }
-                    else
-                    {
-                        CompleteEvent(type);
-                    }
+                    if (ServerManager.Instance.ChannelId == 51) CaligorRaid.Run();
+                    else CompleteEvent(type);
                     break;
-
                 case EventType.ICEBREAKER:
-                    if (ServerManager.Instance.ChannelId != 51)
-                    {
-                        IceBreaker.GenerateIceBreaker(levelBracket);
-                    }
-                    else
-                    {
-                        CompleteEvent(type);
-                    }
+                    if (ServerManager.Instance.ChannelId != 51) IceBreaker.GenerateIceBreaker(levelBracket);
+                    else CompleteEvent(type);
                     break;
-
                 case EventType.WORLDBOSS:
-                    if (ServerManager.Instance.ChannelId != 51)
-                    {
-                        WorldBoss.GenerateWorldBoss();
-                    }
-                    else
-                    {
-                        CompleteEvent(type);
-                    }
+                    if (ServerManager.Instance.ChannelId != 51) WorldBoss.GenerateWorldBoss();
+                    else CompleteEvent(type);
                     break;
-
                 case EventType.ASGOBAS:
-                    if (ServerManager.Instance.ChannelId != 51)
-                    {
-                        AsgobasInstantBattle.GenerateInstantBattle();
-                    }
-                    else
-                    {
-                        CompleteEvent(type);
-                    }
+                    if (ServerManager.Instance.ChannelId != 51) AsgobasInstantBattle.GenerateInstantBattle();
+                    else CompleteEvent(type);
                     break;
-
                 case EventType.AUTOREBOOT:
                     ServerManager.Instance.IsReboot = true;
                     ServerManager.Instance.RebootTask = new Task(ServerManager.Instance.AutoReboot);
                     ServerManager.Instance.RebootTask.Start();
                     break;
-
-                // These values are transported by the cluster contract but do not
-                // currently have a local GameEventHandler implementation. They are
-                // reported explicitly instead of failing silently or remaining stuck
-                // in StartedEvents forever.
                 case EventType.GLACERNONRAID:
                 case EventType.METEORITEGAME:
                 case EventType.Act7Ship:
@@ -196,16 +131,11 @@ namespace NosGm.GameObject.Plugin.Event
                 case EventType.DUELEVENT:
                 case EventType.DUELEVENTPRIVATE:
                 case EventType.OpenWorldBoss:
-                    Logger.Warn(
-                        $"[EVENT_RUNTIME] Event={type} Result=UnsupportedLocalDispatch " +
-                        $"Channel={ServerManager.Instance.ChannelId} Value={value}");
+                    Logger.Warn($"[EVENT_RUNTIME] Event={type} Result=UnsupportedLocalDispatch Channel={ServerManager.Instance.ChannelId} Value={value}");
                     CompleteEvent(type);
                     break;
-
                 default:
-                    Logger.Warn(
-                        $"[EVENT_RUNTIME] Event={type} Result=UnknownEvent " +
-                        $"Channel={ServerManager.Instance.ChannelId}");
+                    Logger.Warn($"[EVENT_RUNTIME] Event={type} Result=UnknownEvent Channel={ServerManager.Instance.ChannelId}");
                     CompleteEvent(type);
                     break;
             }
