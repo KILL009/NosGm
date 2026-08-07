7d2a8db83a3da92e1e4bea9d5a1fca0e6b604ba6
KILL009 <gmthemaster@gmail.com>
2026-08-07 14:55:21 -0400

    Add configuration contract self-test scaffold

---
+++ tests/NosGm.Authentication.Runtime.SelfTest/ClusterConfigurationContractSelfTest.cs
@@ -0,0 +1,10 @@
+using System.Runtime.CompilerServices;
+
+internal static class ClusterConfigurationContractSelfTest
+{
+    [ModuleInitializer]
+    public static void Run()
+    {
+        Console.WriteLine("[PASS] Cluster configuration contract self-test loaded");
+    }
+}
